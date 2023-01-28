8411e2e7d3534577f2fb194f8decf0566753e20a
Fung Oi Lo <folo.mary@gmail.com>
2023-01-28 15:42:39 +0000

    Display title and shortDescription

--- src/controllers/AppController.ts
+++ src/controllers/AppController.ts
@@ -1,7 +1,36 @@
 import { Request, Response } from 'express';
+import axios from 'axios';
+import Jsona from 'jsona';
+import _ from 'lodash';
 
 export class AppController {
-  static render(req: Request, res: Response) {
-    res.render('index');
+
+  static async getApiData() {
+    const API_ENDPOINT: string = process.env.API_ENDPOINT || 'https://www.roh.org.uk/api/event-details?slug=turandot-by-andrei-serban';
+
+    try {
+      const { data } = await axios.get(API_ENDPOINT);
+      const dataFormatter = new Jsona();
+      const eventDetail = dataFormatter.deserialize(data);
+
+      return {
+        title: _.get(eventDetail, 'title', ''),
+        shortDescription: _.get(eventDetail, 'shortDescription', ''),
+      };
+
+    } catch(error) {
+      throw new Error(`Fail to fetch data from ${API_ENDPOINT}`);
+    }
+
+  }
+
+
+  static async render(req: Request, res: Response) {
+    try {
+      const { title, shortDescription } = await AppController.getApiData();
+      res.render('index', { title, shortDescription });
+    } catch(error) {
+      res.render('error', { error });
+    }
   }
 }
